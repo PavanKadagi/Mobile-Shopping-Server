ed7d05cfde20c34c3a6322614c35c2d2f2688072
abc <abc@example.com>
2023-05-15 19:35:20 +0530

    changes

--- DB/connect.js
+++ DB/connect.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB_SERVER).then((res)=>console.log('Connection success..!')).catch((err)=>console.log('No connection',err.message))
+mongoose.connect(process.env.DB_SERVER,{
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then((res)=>console.log('Connection success..!')).catch((err)=>console.log('No connection',err.message))
 
