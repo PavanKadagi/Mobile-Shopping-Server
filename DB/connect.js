d5d78e76dc469daa0359d100be5efdefb36cafe8
abc <abc@example.com>
2023-05-15 19:51:48 +0530

    changes

--- DB/connect.js
+++ DB/connect.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-const url = process.env.CLIENT_URL;
 mongoose.connect(process.env.DB_SERVER,{
     useNewUrlParser: true, useUnifiedTopology: true
-}).then((res)=>console.log('Connection success..!')).catch((err)=>console.log('No connection',err.message)).mongoose.set('useFindAndModify', false)
+}).then((res)=>console.log('Connection success..!')).catch((err)=>console.log('No connection',err.message))
 
